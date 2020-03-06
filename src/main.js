5c53ca55ab8c99546ae44e5f5eba170ae2b8528b
Sebastain Greenholtz <sgreenholtz@armsbusinesssolutions.com>
2020-03-06 11:26:57 -0600

    Not showing up when the window pops up

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@ const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
+
+
 app.on('ready', ()=>{
-    console.log('ready')
-    new BrowserWindow({
+    mainWindow = new BrowserWindow({
         height: 400,
         width: 400
+    })
+
+    mainWindow.loadURL('file://${__dirname}/countdown.html')
+
+    mainWindow.on('closed', ()=>{
+        console.log('Window closed')
     })
 })
